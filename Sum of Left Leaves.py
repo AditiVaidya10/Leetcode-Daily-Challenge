d93c0a2b87dc56af4a15e39755f958e8ee11e836
Aditi Atul Vaidya <aditivaidya10@gmail.com>
2024-04-14 19:22:25 +0530

    Create Sum of Left Leaves.py

---
+++ Sum of Left Leaves.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        ans = 0
+        
+        if root.left:
+            if not root.left.left and not root.left.right:
+                ans += root.left.val
+            else:
+                ans += self.sumOfLeftLeaves(root.left)
+        
+        ans += self.sumOfLeftLeaves(root.right)
+        
+        return ans
