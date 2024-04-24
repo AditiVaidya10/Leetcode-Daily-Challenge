806965a7ea5685a07cf1fa0b839401fbee8f9b82
Aditi Atul Vaidya <aditivaidya10@gmail.com>
2024-04-24 12:21:45 +0530

    Create N-th Tribonacci Number.py

---
+++ N-th Tribonacci Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
