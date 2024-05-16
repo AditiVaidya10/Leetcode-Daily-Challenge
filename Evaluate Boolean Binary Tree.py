62b82c98f628c1041bca39cd3cb275101305f886
Aditi Atul Vaidya <aditivaidya10@gmail.com>
2024-05-16 11:30:11 +0530

    Create Evaluate Boolean Binary Tree.py

---
+++ Evaluate Boolean Binary Tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def helper(self, root):
+        if root.val == 0 or root.val == 1:
+            return root.val == 1
+        elif root.val == 2:
+            return self.helper(root.left) or self.helper(root.right)
+        elif root.val == 3:
+            return self.helper(root.left) and self.helper(root.right)
+        return False
+        
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        return self.helper(root)
