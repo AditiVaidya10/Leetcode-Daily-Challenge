9bd72f8628616f5a9112f7b940004a3288c232f1
Aditi Atul Vaidya <aditivaidya10@gmail.com>
2024-06-05 10:59:49 +0530

    Create Find Common Characters.py

---
+++ Find Common Characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        result = []
+        
+        
+        for char in range(ord('a'), ord('z') + 1):
+            char = chr(char)
+            min_count = float('inf')  
+            
+          
+            for word in words:
+                count = word.count(char)  
+                min_count = min(min_count, count)  
+                if min_count == 0:
+                    break  
+            result.extend([char] * min_count)
+        
+        return result     
